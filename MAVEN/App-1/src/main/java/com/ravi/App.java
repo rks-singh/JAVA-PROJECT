b5c18123603134067e5f9db59dd7399992907cd4
rks-singh <rks884088@gmail.com>
2023-08-29 23:16:21 +0530

    Changes done in App.java ::Task 1

--- MAVEN/App-1/src/main/java/com/ravi/App.java
+++ MAVEN/App-1/src/main/java/com/ravi/App.java
@@ -9,10 +9,12 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-<<<<<<< HEAD
-	System.out.prinltn("Hello my name is Ravi Singh");
-=======
-        System.out.prinltn("My name is Ravi Singh");
->>>>>>> 187b5129a3eafd74601b70e95c178f9bfc788989
+	System.out.prinltn("Hello my name is Ravi Sing");
+	System.out.prinltn("My name is Ravi Singh");
+
+	int x =10;
+	int y =30;
+	int z = x+y;
+	System.out.println("Sum ::"+z);
     }
 }
