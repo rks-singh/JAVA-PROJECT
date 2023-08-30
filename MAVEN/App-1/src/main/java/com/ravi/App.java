9e21c601434ad37502cfa022ae0e24bd3a937c92
= <=>
2023-08-30 13:41:26 +0530

    Changes in App.java

--- MAVEN/App-1/src/main/java/com/ravi/App.java
+++ MAVEN/App-1/src/main/java/com/ravi/App.java
@@ -11,10 +11,12 @@ public class App
         System.out.println( "Hello World!" );
 	System.out.prinltn("Hello my name is Ravi Sing");
 	System.out.prinltn("My name is Ravi Singh");
+	System.out.println("I am from Bihar");
 
 	int x =10;
 	int y =30;
 	int z = x+y;
 	System.out.println("Sum ::"+z);
+
     }
 }
